01d27b9d67e3432f3e34de08adbf21175d9941af
Clodagh <clodaghdo@gmail.com>
2021-06-08 19:34:31 +0100

    Test 1

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\draftery\OneDrive - ESRI (UK) Ltd\Clodagh DA Course\Pandas\netflix_titles.csv")
+print(df.shape)
+print(df.head())
